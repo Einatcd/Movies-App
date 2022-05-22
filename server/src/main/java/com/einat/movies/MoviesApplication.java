12f143bd36d4ce0d865c1d562a4c1f17bad49710
Einat Sidi <einet6@gmail.com>
2022-05-22 23:33:10 +0300

    configure run files

--- server/src/main/java/com/einat/movies/MoviesApplication.java
+++ server/src/main/java/com/einat/movies/MoviesApplication.java
@@ -3,10 +3,6 @@ package com.einat.movies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import com.einat.movies.mongoDB.Movie;
-
-//import org.springframework.boot.CommandLineRunner;
-
 @SpringBootApplication
 public class MoviesApplication{
 		
